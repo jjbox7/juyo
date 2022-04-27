4035cef2f27c7b879d0d333c10817362eda1b004
jjbox7 <jjbox7@naver.com>
2022-04-27 09:49:40 +0900

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,5 +1,6 @@
 // let str = '100'
 // let num = 100
+// AND 연산자
 // console.log(str === num)
 // console.log(true && true)
 // console.log(true && false)
@@ -14,10 +15,62 @@
 // console.log(!isStudent && isFemale)
 // console.log(!isStudent && !isFemale)
 
-let isStudent = true;
-let isFemale = false;
 
-console.log(isStudent && isFemale)
-console.log(isStudent && !isFemale)
-console.log(!isStudent && isFemale)
-console.log(!isStudent && !isFemale)
+// let isStudent = true;
+// let isFemale = false;
+
+// console.log(isStudent && isFemale)
+// console.log(isStudent && !isFemale)
+// console.log(!isStudent && isFemale)
+// console.log(!isStudent && !isFemale)
+// AND 연산자(&&)를 사용했을 경우 두 조건이 모두 truthy 해야만 true, 그렇지 않으면 false가 출력됩니다.
+
+// OR 연산자
+
+// console.log(true || true)
+// console.log(true || false)
+// console.log(false || true)
+// console.log(false || false)
+
+// let isStudent = true;
+// let isFemale = true;
+
+// console.log(isStudent || isFemale)
+// console.log(isStudent || !isFemale)
+// console.log(!isStudent || isFemale)
+// console.log(!isStudent || !isFemale)
+
+// let isStudent = true;
+// let isFemale = false;
+
+// console.log(isStudent || isFemale)
+// console.log(isStudent || !isFemale)
+// console.log(!isStudent || isFemale)
+// console.log(!isStudent || !isFemale)
+// OR연산자(||)를 사용했을 경우 두 조건 중 하나라도 truthy한 값일 경우 true, 그렇지 않으면 false가 출력됩니다.
+
+// // NOT 연산자
+
+// console.log(!true)
+// console.log(!false)
+
+// console.log(!true)
+// console.log(!false)
+// let isStudent = true;
+// let isFemale = false;
+
+// console.log(!isStudent)
+// console.log(!isFemale)
+
+// let isStudent = true;
+// let isFemale = false;
+
+// console.log(!!isStudent)
+// console.log(!!isFemale)
+
+// let isStudent = true;
+// let isFemale = false;
+
+// console.log(!!!isStudent)
+// console.log(!!!isFemale)
+// NOT연산자는 결과가 true이면 false를, false이면 true를 반환합니다. 2개 이상을 중첩하는 것도 가능합니다.
