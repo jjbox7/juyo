26a4814532439d578a7b48f12e876cc6720ff155
jjbox7 <jjbox7@naver.com>
2022-04-27 09:43:17 +0900

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,2 +1,23 @@
-console.log('hello');
-console.log('juyoung');
+// let str = '100'
+// let num = 100
+// console.log(str === num)
+// console.log(true && true)
+// console.log(true && false)
+// console.log(false && true)
+// console.log(false && false)
+
+// let isStudent = true;
+// let isFemale = true;
+
+// console.log(isStudent && isFemale)
+// console.log(isStudent && !isFemale)
+// console.log(!isStudent && isFemale)
+// console.log(!isStudent && !isFemale)
+
+let isStudent = true;
+let isFemale = false;
+
+console.log(isStudent && isFemale)
+console.log(isStudent && !isFemale)
+console.log(!isStudent && isFemale)
+console.log(!isStudent && !isFemale)
