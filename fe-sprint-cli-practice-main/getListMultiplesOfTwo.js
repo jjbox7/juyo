f1e4fd85b0fb333f166ed72336e85a81f52a4699
jjbox7 <100186616+jjbox7@users.noreply.github.com>
2022-05-09 15:42:45 +0900

    git practice

---
+++ fe-sprint-cli-practice-main/getListMultiplesOfTwo.js
@@ -0,0 +1,9 @@
+const { range } = require('range'); // range 모듈을 불러옵니다
+
+function getListMultiplesOfTwo(upTo) {
+  // TODO
+  return range(2, upTo+2, 2);
+}
+
+module.exports = getListMultiplesOfTwo;
+let = 25;
