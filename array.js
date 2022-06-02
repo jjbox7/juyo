612e6192a1d9afd27565df9ca026898bb67cf2ad
jjbox7 <jjbox7@naver.com>
2022-06-02 10:09:25 +0900

    Update array.js

--- array.js
+++ array.js
@@ -1,6 +1,20 @@
-let num = [10,20,30,40,10];
-let sum = 0;
-
-for (let sum=0; sum+num; n++){
-    console.log(num[sum]);
-}
+function makeAddressBook(addressBook, user) {
+    let firstLetter = user.name[0];
+  
+    if(firstLetter in addressBook) {
+      addressBook[firstLetter].push(user);
+    } else {
+      addressBook[firstLetter] = [];
+      addressBook[firstLetter].push(user);
+    }
+  
+    return addressBook;
+  }
+  
+  let users = [
+    { name: 'Tim', age: 40 },
+    { name: 'Satya', age: 30 },
+    { name: 'Sundar', age: 50 }
+  ];
+  
+  users.reduce(makeAddressBook, {});
