d1de3d81b9684dc3ecc45ac287480d455d0e45c6
jjbox7 <jjbox7@naver.com>
2022-06-11 17:30:10 +0900

    TEST

--- reetcode.js
+++ reetcode.js
@@ -38,4 +38,16 @@ function insertDash(str) {
   
     return result;
   }
+  function insertDash(str) {
+    let result = str[0];
+    for (let i = 1; i < str.length; i++) {
+      if (Number(str[i - 1]) % 2 && Number(str[i]) % 2) {
+        result = result + '-';
+      }
+      result = result + str[i];
+    }
   
+    return result;
+  }
+  
+
