7b2908ccec9a7795ec04ac68e2e736224c53eab4
jjbox7 <jjbox7@naver.com>
2022-07-18 08:37:12 +0900

    test

--- fe-sprint-calculator-main/script.js
+++ fe-sprint-calculator-main/script.js
@@ -478,3 +478,34 @@ function quickSort(arr, transform = (item) => item) {
   const rSorted = quickSort(right, transform);
   return [...lSorted, pivot, ...rSorted];
 }
+
+const merge = function (left, right) {
+  let merged = [];
+  let leftIdx = 0,
+    rightIdx = 0;
+  const size = left.length + right.length;
+
+  for (let i = 0; i < size; i++) {
+    if (leftIdx >= left.length) {
+      merged.push(right[rightIdx]);
+      rightIdx++;
+    } else if (rightIdx >= right.length || left[leftIdx] <= right[rightIdx]) {
+      merged.push(left[leftIdx]);
+      leftIdx++;
+    } else {
+      merged.push(right[rightIdx]);
+      rightIdx++;
+    }
+  }
+
+  return merged;
+};
+
+const mergeSort = function (arr) {
+  if (arr.length < 2) return arr;
+  const middle = parseInt(arr.length / 2);
+  const left = mergeSort(arr.slice(0, middle));
+  const right = mergeSort(arr.slice(middle));
+  const merged = merge(left, right);
+  return merged;
+};
