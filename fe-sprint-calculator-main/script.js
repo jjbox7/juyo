294f95ffe58affbbf4a39c2ca96bda923e39bc10
jjbox7 <jjbox7@naver.com>
2022-08-15 19:01:04 +0900

    test

--- fe-sprint-calculator-main/script.js
+++ fe-sprint-calculator-main/script.js
@@ -1742,3 +1742,107 @@ function divideChocolateStick(M, N) {
 
   return result;
 }
+for (let dst = src + 1; dst <= end; dst++) {
+  const dist = calculateDistance(sorted[src], sorted[dst]);
+  min = Math.min(min, dist);
+}
+}
+return min;
+};
+
+const closestCrossing = (mid, sorted, min) => {
+// 가운데(mid)를 기준으로
+const strip = [];
+const midX = sorted[mid][1];
+let lIdx = mid - 1;
+let rIdx = mid + 1;
+
+// 왼쪽과 오른쪽 부분에서 가장 가까운 두 점 사이의 거리가 min으로 주어진다.
+// 가운데를 기준으로 오직 x좌표만을 기준으로 min보다 가까운 좌표만 고려한다.
+// y좌표가 같다고 가정하면, 최소한 이 조건(x좌표 기준 min 이하)을 만족해야하기 때문이다.
+// y좌표가 같을 경우 두 점 사이의 거리는 x축 좌표 간의 거리다.
+// 단, 소수점 계산을 피하기 위해 두 점 사이의 거리에 100을 곱하고 있으므로 동일한 기준을 적용해야 한다.
+
+// sorted는 x축을 기준으로 정렬되어 있기 때문에,
+// mid를 기준으로 가까운 거리부터 최소 기준(min보다는 가까워야 함)을 만족할 때까지만 탐색을 하면 된다.
+while (
+rIdx < sorted.length &&
+Math.abs(midX - sorted[rIdx][1]) * 100 < min
+) {
+rIdx++;
+}
+while (lIdx >= 0 && Math.abs(midX - sorted[lIdx][1]) * 100 < min) {
+lIdx--;
+}
+
+// while 탈출하기 위한 조건을 보면,
+// lIdx는 1을 더해야 하고, rIdx는 1을 줄여야 한다.
+// 아래 구간에 대해서 brute force를 적용한다.
+for (let i = lIdx + 1; i < rIdx; i++) {
+for (let j = i + 1; j < rIdx; j++) {
+  min = Math.min(min, calculateDistance(sorted[i], sorted[j]));
+}
+}
+return min;
+};
+
+const closestFrom = (start, end, size, sorted) => {
+if (size <= 3) {
+// 최소 두 개 이상의 점이 있어야 거리를 계산할 수 있다.
+//  1) 모든 점의 개수가 4개일 경우, 각각 2개로 나눌 수 있다.
+//  2) 모든 점의 개수가 5개일 경우, 각각 2, 3개로 나눌 수 있다. 3개인 경우 더 나눌 수 없다.
+return bruteForce(start, end, sorted);
+}
+// 가운데를 기준으로 분할한 뒤 재귀적으로 문제를 해결한다.
+const mid = Math.floor((start + end) / 2);
+// 왼쪽, 오른쪽으로 나뉜 부분에서 각각 가장 가까운 두 점 사이의 거리를 구한다.
+const minLeft = closestFrom(start, mid, mid - start + 1, sorted);
+const minRight = closestFrom(mid + 1, end, end - mid, sorted);
+
+// 전체 영역에서 가장 가까운 두 점 사이의 거리는 아래 중 하나다.
+//  1) 위에서 구한 두 거리(minLeft, minRight)
+//  2) 가운데를 가로지르는 두 점 사이의 거리
+// 먼저 1)중에서 더 짧은 거리를 구한다. 최종 정답은 이보다 작거나 같아야 한다.
+let min = Math.min(minLeft, minRight);
+return closestCrossing(mid, sorted, min);
+};
+
+// x좌표를 기준으로 정렬한다.
+const sorted = mergeSort(points.slice(0), (item) => item[1]);
+return closestFrom(0, sorted.length - 1, sorted.length, sorted);
+};
+// 최대 공약수(유클리드 호제법: Euclidean algorithm)
+function gcd(m, n) {
+if (m % n === 0) return n;
+return gcd(n, m % n);
+}
+
+function divideChocolateStick(M, N) {
+const result = [];
+// 최대공약수를 구한다.
+// M, N의 순서는 상관없다.
+const GCD = gcd(M, N);
+let temp = 0; //
+
+// 약수는 대칭적이므로 제곱근까지만 반복해도 된다.
+// 예) 36의 약수는 1, 2, 3, 4, 6, 9, 12, 18, 36이다.
+// 제곱근을 기준으로 양쪽의 값 하나씩 곱했을 때 36이 되기 때문에
+// 제곱근 보다 큰 약수는 제곱근보다 작은 약수에서 구할 수 있다.
+const sqrt = Math.floor(Math.sqrt(GCD));
+for (let left = 1; left <= sqrt; left++) {
+if (GCD % left === 0) {
+// 최대공약수의 약수인 경우 중 제곱근 보다 작은 약수의 경우
+result.push([left, M / left, N / left]);
+if (left * left < GCD) {
+  // 제곱근이 아닌 경우(제곱근 보다 작은)
+  right = GCD / left; // 최대 공약수를 제곱근이 아닌 수로 나누면 제곱근 보다 큰 약수를 구할 수 있다.
+  result.push([right, M / right, N / right]);
+}
+}
+}
+
+// '빼빼로를 받게 되는 직원의 수'를 기준으로 오름차순으로 정렬
+result.sort((op1, op2) => op1[0] - op2[0]);
+
+return result;
+}
