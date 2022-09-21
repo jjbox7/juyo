bf0b3329ffe5dfdb3fce9ca1a50b634c666313fb
jjbox7 <jjbox7@naver.com>
2022-09-21 23:13:19 +0900

    test

--- test2.js
+++ test2.js
@@ -743,4 +743,22 @@ class GraphWithAdjacencyMatrix {
     // this.matrix[from][to]의 값을 0으로 바꿔줘서 관련이 없음을 표시합니다.
     this.matrix[from][to] = 0;
   }
+}function mostFrequentCharacter(str) {
+  let obj = { mostCount: 0, mostFrequent: '' };
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === ' ') {
+      continue;
+    }
+
+    if (obj[str[i]] === undefined) {
+      obj[str[i]] = 0;
+    }
+    obj[str[i]] += 1;
+
+    if (obj[str[i]] > obj['mostCount']) {
+      obj['mostCount'] = obj[str[i]];
+      obj['mostFrequent'] = str[i];
+    }
+  }
+  return obj['mostFrequent'];
 }
